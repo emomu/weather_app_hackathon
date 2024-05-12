506a345baab6d29488df3c335f021e44c7f2fdc1
Berkhan Cak <berkhan875@gmail.com>
2024-05-12 12:16:38 +0300

    added scaffold

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -10,6 +10,6 @@ class Mainpage extends StatefulWidget {
 class _MainpageState extends State<Mainpage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
